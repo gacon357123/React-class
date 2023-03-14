2fbe04a8f0e246faf69c1d7c530cf66367e9597c
Nguyen Quoc Bao <gacon357123@gmail.com>
2023-03-14 09:40:22 +0700

    redux

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.scss";
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>redux</h1>
+      </header>
+    </div>
+  );
+};
+
+export default App;
